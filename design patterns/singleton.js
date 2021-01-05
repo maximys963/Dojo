d20a380ed5820b3a98f9c64c7ea78e3884baf437
Max Panasenko <maxim.p.jsdev@gmail.com>
2021-01-05 13:12:34 +0200

    singleton

---
+++ design patterns/singleton.js
@@ -0,0 +1,18 @@
+class Star {
+    constructor(name) {
+        if(Star.isExists){
+            return Star.instance
+        }
+
+        Star.instance = this
+        Star.isExists = true
+        this.name = name
+    }
+}
+
+const sun = new Star('Sun');
+
+const somethingElse = new Star('Sirius');
+
+console.log(sun);
+console.log(somethingElse)
