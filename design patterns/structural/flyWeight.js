873fd7ee1ea56a9b80673bed0f07eb60ad8a6b84
Max Panasenko <maxim.p.jsdev@gmail.com>
2021-01-05 19:00:05 +0200

    add flyweight

---
+++ design patterns/structural/flyWeight.js
@@ -0,0 +1,51 @@
+class Question{
+    constructor(text, answer){
+        this.text = text;
+        this.answer = answer
+    }
+}
+
+class QuestionFactory{
+    constructor(){
+        this.questions = []
+    }
+
+    getAllQuestion(){
+        return this.questions
+    }
+
+    getQuestionsWithAnswer(text){
+
+        if (this.questions.length !== 0) {
+            const userQuestion = this.questions.find((question) => question.text === text)
+
+            if(userQuestion){
+                return userQuestion
+            }
+        }
+
+        const newQuestion = this.longRequestForQuestion(text);
+
+        this.questions.push(newQuestion)
+        return newQuestion
+    }
+
+    longRequestForQuestion(text){
+        return new Question(text, 'this is answer')
+    }
+}
+
+const factory = new QuestionFactory();
+
+
+const question1 = factory.getQuestionsWithAnswer('lorem');
+const question2 = factory.getQuestionsWithAnswer('ipsum');
+const question3 = factory.getQuestionsWithAnswer('lorem');
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
+
+const allQuestion = factory.getAllQuestion();
+console.log('allQuestion', allQuestion);
+
